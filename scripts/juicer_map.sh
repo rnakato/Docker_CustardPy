811f01c403c10314441874f6c86fb23c218e5677
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-06-02 14:59:37 +0900

    add juicebox.jar

--- scripts/juicer_map.sh
+++ scripts/juicer_map.sh
@@ -8,13 +8,13 @@ function usage()
     echo '   <build>: genome build (e.g., hg38)' 1>&2
     echo '   <gt>: genome table' 1>&2
     echo '   <bwaindex>: index file of BWA' 1>&2
-    echo '   <enzyme>: enzyme' 1>&2
+    echo '   <enzyme>: enzyme (e.g., HindIII, MboI)' 1>&2
     echo '   <fastq_post [_|_R]>: if the filename of fastqs is *_[1|2].fastq, supply "_". if *_[R1|R2].fastq, choose "_R".' 1>&2
     echo '   Options:' 1>&2
     echo '      -p ncore: number of CPUs (default: 32)' 1>&2
     echo '      -m tmpdir: tempdir' 1>&2
     echo '   Example:' 1>&2
-    echo "      $cmdname $(pwd)/fastq/Hap1-A/ $(pwd)/JuicerResults/Hap1-A hg38 genometable.hg38.txt bwaindex/hg38 _R" 1>&2
+    echo "      $cmdname $(pwd)/fastq/Hap1-A/ $(pwd)/JuicerResults/Hap1-A hg38 genometable.hg38.txt bwaindex/hg38 HindIII _R" 1>&2
 }
 
 tmpdir=""

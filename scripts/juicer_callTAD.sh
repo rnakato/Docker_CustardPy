41b29934240960a5a52fde91dcca8a3511805206
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-06-08 17:38:32 +0900

    modify scripts/juicer_callTAD.sh

--- scripts/juicer_callTAD.sh
+++ scripts/juicer_callTAD.sh
@@ -2,27 +2,46 @@
 cmdname=`basename $0`
 function usage()
 {
-    echo "$cmdname <norm> <hic> <odir> <gt>" 1>&2
+    echo "$cmdname [options] <norm> <odir> <hic> <gt>" 1>&2
+    echo '   <norm>: normalization type (NONE|VC|VC_SQRT|KR|SCALE)' 1>&2
+    echo '   <odir>: output directory (e.g., "JuicerResults/sample1")' 1>&2
+    echo '   <hic>: .hic file' 1>&2
+    echo '   <gt>: genome table' 1>&2
+    echo '   Options:' 1>&2
+    echo '     -r resolutions: the resolutions for ArrowHead (default: "10000 25000 50000", should be quoted and separated by spaces)' 1>&2
+    echo '     -p ncore: number of CPUs (default: 24)' 1>&2
 }
+
+resolutions="10000 25000 50000"
+ncore=24
+while getopts r:p: option; do
+    case ${option} in
+        r) resolutions=${OPTARG} ;;
+	p) ncore=${OPTARG} ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND - 1))
 
 if [ $# -ne 4 ]; then
   usage
   exit 1
 fi
 
-
-
 norm=$1
-hic=$2
-odir=$3
+odir=$2
+hic=$3
 gt=$4
 
 dir=$odir/TAD/$norm
 mkdir -p $dir
 
-for res in 10000 25000 50000; do
+for res in $resolutions; do
     if test ! -e $dir/${res}_blocks.bedpe; then
-        juicertools.sh arrowhead -m 2000 -r $res --threads 24 -k $norm $hic $dir # --ignore_sparsity
+        juicertools.sh arrowhead -m 2000 -r $res --threads $ncore -k $norm $hic $dir
     fi
 
     # make TAD bed
@@ -47,11 +66,4 @@ for res in 10000 25000 50000; do
 
     # non-TAD region
     cat $dir/${res}_blocks.TADcoverage.bed | awk '{if($4==0) print}' | cut -f1-3 | sort -k1,1 -k2,2n > $dir/${res}_blocks.nonTADregions.bed
-
-#    tmpfile1=$(mktemp "/tmp/${0##*/}.tmp.XXXXXX")
-#    tmpfile2=$(mktemp "/tmp/${0##*/}.tmp.XXXXXX")
-#    cat $dir/${res}_blocks.merged.bed | awk -v res=$res '{OFS="\t"} NR>1 {printf "%s\t%d\t%d\n", $1, $2, $3+res }' > $tmpfile1
-#    sort -k1,1 -k2,2n $gt > $tmpfile2
-#    bedtools complement -i $tmpfile1 -g $tmpfile2 > $dir/${res}_blocks.nonTADregions.bed
-#    rm $tmpfile1 $tmpfile2
 done

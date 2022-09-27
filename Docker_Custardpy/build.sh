8d80ab4658cdea67a37d4b7200a26c0960cbca21
Ryuichiro Nakato <rnakato@iam.u-tokyo.ac.jp>
2022-09-27 18:28:57 +0900

    v0.2.0

--- Docker_Custardpy/build.sh
+++ Docker_Custardpy/build.sh
@@ -1,6 +1,6 @@
-for tag in 0.1.0
+for tag in 0.2.0 #0.1.0
 do
-    docker build -f Dockerfile.$tag -t rnakato/custardpy:$tag .
+    docker build -f Dockerfile.$tag -t rnakato/custardpy:$tag . #--no-cache
     docker push rnakato/custardpy:$tag
     docker build -f Dockerfile.$tag -t rnakato/custardpy .
     docker push rnakato/custardpy
